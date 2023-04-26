22937f57ebc407cc75ae82aeb1807b8f13864326
Brandon Campbell-Kearns <brandon.j.kearns@gmail.com>
2023-04-26 09:28:11 -0400

    Configures authentication

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const session = require('express-session');
+const passport = require('passport');
+require('./config/passport')(passport);
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const authRouter = require('./routes/auth')
 
 var app = express();
 
@@ -13,7 +18,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+    secret: 'secret_session_key',
+    resave: false,
+    saveUninitialized: false,
+}))
 
+app.use('/auth', authRouter);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
