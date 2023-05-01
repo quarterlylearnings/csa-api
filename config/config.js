0a53540be470994057f113d5a9f7835e6594bfb1
Brandon Campbell-Kearns <brandon.j.kearns@gmail.com>
2023-05-01 11:32:06 -0400

    Adds environment variable

--- config/config.js
+++ config/config.js
@@ -19,7 +19,6 @@ module.exports = {
     "username": process.env.DATABASE_USER,
     "password": process.env.DATABASE_PASSWORD,
     "database": process.env.DATABASE_NAME,
-    "host": "127.0.0.1",
-    "dialect": "postgres"
+    "host": process.env.DATABASE_HOST
   }
 }
