1a26a8cf5ac9ab0d5ecb8ae7ebcc04b876cd1db6
Brandon Campbell-Kearns <brandon.j.kearns@gmail.com>
2023-05-01 11:38:06 -0400

    Adds dialect like my students told me :face_with_rolling_eyes:

--- config/config.js
+++ config/config.js
@@ -19,6 +19,7 @@ module.exports = {
     "username": process.env.DATABASE_USER,
     "password": process.env.DATABASE_PASSWORD,
     "database": process.env.DATABASE_NAME,
-    "host": process.env.DATABASE_HOST
+    "host": process.env.DATABASE_HOST,
+    "dialect": "postgres"
   }
 }
