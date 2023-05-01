c27b80fbb2f3a3620fc09a2bcacde9bed8ee5081
Brandon Campbell-Kearns <brandon.j.kearns@gmail.com>
2023-05-01 11:16:43 -0400

    Adds production environment variables

--- config/config.js
+++ config/config.js
@@ -16,9 +16,9 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
+    "username": process.env.DATABASE_USER,
+    "password": process.env.DATABASE_PASSWORD,
+    "database": process.env.DATABASE_NAME,
     "host": "127.0.0.1",
     "dialect": "postgres"
   }
