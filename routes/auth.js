6b31f33b94919b42e267bca487b0d6320153bdf0
Brandon Campbell-Kearns <brandon.j.kearns@gmail.com>
2023-05-01 23:45:32 -0400

    Adds forgotten require for jwt and removes passport

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
-const passport = require("passport");
+const jwt = require('jsonwebtoken');
 const { User } = require("../models");
 
 router.post("/register", async (req, res) => {
