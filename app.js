a4c7c10ef4c2c950a1c0a6ec9917d6998ad77811
Brandon Campbell-Kearns <brandon.j.kearns@gmail.com>
2023-05-01 23:19:02 -0400

    Removes require statement for sessions module

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const session = require('express-session');
 const passport = require('passport');
 require('./config/passport')(passport);
 
