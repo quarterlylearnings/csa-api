fca84225baa5f82c1ac9a8b64b47d2655eb18350
Brandon Campbell-Kearns <brandon.j.kearns@gmail.com>
2023-05-01 23:24:44 -0400

    Modifies auth middlewares to use the jwt authentication method

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,22 +1,7 @@
 const passport = require('passport');
 
 function authenticate(req, res, next) {
-    passport.authenticate('local', (error, user) => {
-        if (error) {
-            return next(error);
-        }
-
-        if(!user) {
-            return res.status(401).json({ message: 'Incorrect username or password.' })
-        }
-
-        req.logIn(user, (error) => {
-            if(error) {
-                return next(error);
-            }
-            next()
-        });
-    })(req,res,next);
+    passport.authenticate('jwt', { session: false })(req,res,next);
 }
 
 module.exports = { authenticate };
