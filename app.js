082dea4211c7d5dd4e06273c5a1e9553220f08bd
Brandon Campbell-Kearns <brandon.j.kearns@gmail.com>
2023-05-01 23:18:22 -0400

    Initialize passport without sessions

--- app.js
+++ app.js
@@ -19,11 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({
-    secret: 'secret_session_key',
-    resave: false,
-    saveUninitialized: false,
-}))
+app.use(passport.initialize())
 
 app.use('/auth', authRouter);
 app.use('/', indexRouter);
