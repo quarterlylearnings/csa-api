44c1b8045d7edeb22a81c1b141bb33fa8d3981d4
Brandon Campbell-Kearns <brandon.j.kearns@gmail.com>
2023-05-01 23:33:49 -0400

    Modifies login to return token upon successful cred match

--- routes/auth.js
+++ routes/auth.js
@@ -20,29 +20,26 @@ router.post("/register", async (req, res) => {
   }
 });
 
-router.post("/login", (req, res, next) => {
-  passport.authenticate("local", (err, user, info) => {
-    if (err) {
-      return res.status(500).json({
-        message: "An error occurred during authentication",
-        error: err,
-      });
-    }
+router.post("/login", async (req, res, next) => {
+  try {
+    const user = await User.findOne({ where: { username: req.body.username } });
 
     if (!user) {
-      return res.status(401).json({ message: "There was an error with the user" });
+      return res.status(401).json({ error: 'Invalid username or password' });
     }
 
-    req.login(user, (err) => {
-      if (err) {
-        return res
-          .status(500)
-          .json({ message: "An error occurred during login", error: err });
-      }
+    const isPasswordValid = await bcrypt.compare(req.body.password, user.password);
 
-      return res.status(200).json({ message: "Login successful" });
-    });
-  })(req, res, next);
+    if (!isPasswordValid) {
+      return res.status(401).json({ error: 'Invalid username or password' });
+    }
+
+    const token = jwt.sign({ id: user.id }, 'your_jwt_secret', { expiresIn: '6h' });
+
+    res.status(200).json({ token });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 router.post("/logout", (req, res) => {
